113543385116d6d60c051e18430ef72bae6fe9a9
Ayuilos <ekklovepeace@gmail.com>
2023-08-02 17:55:41 +0800

    fix: use `noopStorage` in createConfig to prevent getting error when page is loading in sandbox iframe
    This is because if `sandbox` doesn't get "allow-same-origin" flag, try read `localStorage` will throw error

--- app/wagmiProvider.tsx
+++ app/wagmiProvider.tsx
@@ -1,19 +1,27 @@
-"use client";
+"use client"
 
-import { WagmiConfig, createConfig, configureChains } from "wagmi";
-import { publicProvider } from "wagmi/providers/public";
-import { chains } from "./chains";
+import { noopStorage } from "@wagmi/core"
+import {
+  configureChains,
+  createConfig,
+  createStorage,
+  WagmiConfig,
+} from "wagmi"
+import { publicProvider } from "wagmi/providers/public"
+
+import { chains } from "./chains"
 
 export const { publicClient, webSocketPublicClient } = configureChains(chains, [
   publicProvider(),
-]);
+])
 
 const config = createConfig({
   autoConnect: true,
+  storage: createStorage({ storage: noopStorage }),
   publicClient,
   webSocketPublicClient,
-});
+})
 
 export default function WagmiProvider({ children }: React.PropsWithChildren) {
-  return <WagmiConfig config={config}>{children}</WagmiConfig>;
+  return <WagmiConfig config={config}>{children}</WagmiConfig>
 }
